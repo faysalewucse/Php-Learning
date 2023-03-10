58f28ad017a285f0437a8db582b1273832879055
faysalewucse <faysal.ewucse@gmail.com>
2023-03-10 15:00:33 +0600

    learn about git branch and make php small project name madlibs

--- index.php
+++ index.php
@@ -9,12 +9,32 @@
 </head>
 
 <body>
-    <form action="index.php" method="get">
+    <!-- <form action="index.php" method="get">
         <input type="text" name="num1"><br>
         <input type="text" name="num2"><br>
         <input type="submit" value="Submit">
     </form>
-    Sum is: <?php echo $_GET["num1"] + $_GET["num2"]; ?><br>
+    Sum is: <?php echo $_GET["num1"] + $_GET["num2"]; ?><br> -->
+
+    Mad Libs Game:
+    <hr>
+    <form action="index.php" method="get">
+        Color: <input type="text" name="color"><br>
+        Plural Noun: <input type="text" name="pluralNoun"><br>
+        Celibrity: <input type="text" name="celibrity"><br>
+        <input type="submit" value="Submit"><br>
+
+        <?php
+        $color = $_GET["color"];
+        $pluralNoun = $_GET["pluralNoun"];
+        $celibrity = $_GET["celibrity"];
+
+        echo "Roses are $color </br>";
+        echo "$pluralNoun Are Blue </br>";
+        echo "I Love $celibrity </br>";
+        ?>
+    </form>
+
 </body>
 
 </html>
